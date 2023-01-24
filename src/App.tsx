15d699f69d3599e4a05a67eba5be4560625e0952
Eddiebee <edbeeotudor@gmail.com>
2023-01-24 10:32:48 +0100

    Remove unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import lendsqrLogo from "./assets/lendsqr.svg";
-import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://www.lendsqr.com/" target="_blank">
-          <img src={lendsqrLogo} className="logo lendsqr" alt="Lendsqr logo" />
-        </a>
-      </div>
       <h1>Lendsqr FE Test</h1>
     </div>
   );
